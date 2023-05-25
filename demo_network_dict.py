28b98fd59081357e394279dac344f0f02232902b
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2023-05-25 04:06:10 +0100

    demos for network_dict; modify other demo

--- demo_network_dict.py
+++ demo_network_dict.py
@@ -1,4 +1,4 @@
-"""File contains a demonstration of how to parse devices."""
+"""File contains a demonstration of how to construct a dict of network."""
 from names import Names
 from scanner import Scanner
 from devices import Devices
@@ -10,11 +10,11 @@ names_1 = Names()
 device_1 = Devices(names_1)
 network_1 = Network(names_1, device_1)
 monitor_1 = Monitors(names_1, device_1, network_1)
-scanner_1 = Scanner("demo_dev.txt", names_1)
+scanner_1 = Scanner("demo_net_dict1.txt", names_1)  # Another file: demo_net_dict2.txt
 
 list_of_symbols_1 = scanner_1.get_list_of_symbols()
 
-print("Contents of demo_dev.txt")
+print("Contents of demo_net_dict1.txt")
 for symbol in list_of_symbols_1:
     id = symbol.id
     name = names_1.get_name_string(id)
@@ -23,6 +23,6 @@ for symbol in list_of_symbols_1:
     print(name, end=" ")
 print()
 
-# Check Parser.device(), Parser.device_list()
+# Check Parser.network_dict()
 parser_1 = Parser(names_1, device_1, network_1, monitor_1, scanner_1)
-print(parser_1.device_list())
+print(parser_1.network_dict())
