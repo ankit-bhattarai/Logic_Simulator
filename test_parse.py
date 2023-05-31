168956963ea107953fa95c31dc80a56921f95717
adh1s <aps85@cam.ac.uk>
2023-05-31 16:44:23 +0100

    Build network dict tests in test_parse.py

--- test_parse.py
+++ test_parse.py
@@ -86,7 +86,7 @@ def new_monitors(new_names, new_devices, new_network):
                          [("test_parse_files/syntax_error_9.txt", [1, 0, 2], [3, 7, 11], [7, 0, 1])])
 def test_name_error_identification(definition_file, name_error_indices, error_symbol_indices,
                                    letter_indices, new_names, new_devices, new_network, new_monitors):
-    """Test that the correct syntax error is identified."""
+    """Test that the correct syntax error (name error) is identified at the correct locations."""
     new_scanner = Scanner(definition_file, new_names)
     new_scanner.print_error = MagicMock()
     new_parser = Parser(
@@ -102,7 +102,7 @@ def test_name_error_identification(definition_file, name_error_indices, error_sy
     for name_error_index, symbol_index, letter_index in zip(name_error_indices,
                                                             error_symbol_indices,
                                                             letter_indices):
-        print(name_error_index)
+
         erronous_symbol = new_scanner.list_of_symbols[symbol_index]
         error_message = syntax_error_types[9] + name_errors[name_error_index]
         expected_call = call(erronous_symbol, letter_index, error_message)
@@ -137,8 +137,11 @@ def test_early_termination_error_identification(definition_file, new_names,
     new_parser.network_dict()
 
     erronous_symbol = new_scanner.list_of_symbols[-1]
+    # Check that the print error function is called only once and with the correct error message/location
     new_scanner.print_error.assert_called_once_with(
         erronous_symbol, 0, syntax_error_types[22])
+    # Check that this is the only error that has been accumulated by num of errors 
+    assert new_parser.num_of_errors == 1
 
 
 @pytest.mark.parametrize("definition_file, error_type, error_symbol_indices",
@@ -201,9 +204,11 @@ def test_syntax_error_identification(definition_file, error_type, error_symbol_i
     new_scanner.print_error.assert_has_calls(expected_calls, any_order=False)
     # Assert that no other errors are passed for printing
     assert new_scanner.print_error.call_count == len(error_symbol_indices)
+    # Assert that the internally stored number of errors is correct
+    assert new_parser.num_of_errors == len(error_symbol_indices)
 
 @pytest.fixture
-def network_dict_1(new_names):
+def network_dict_1():
     network_dict = {}
     network_dict['DEVICES'] = [[Symbol("CLOCK", 5, 1, 10),
                                Symbol("clock1", 16, 1, 16),
@@ -218,9 +223,10 @@ def network_dict_1(new_names):
     return network_dict
 
 def test_build_network_dict_1(new_names, network_dict_1, new_devices, new_network,
-                            new_monitors):
-    """Test that the network dictionary, used later to check semantics and
-        to build the network, is built correctly."""
+                              new_monitors):
+    """Test that the network dictionary (when CORRECT syntax is passed, used later to
+       check semantics and to build the network, is built correctly - for increasingly
+       complex files - through test_build_network_dict_{1, 2, 3}."""
     new_scanner = Scanner("test_parse_files/test_build_network_dict_1.txt", new_names)
     new_parser = Parser(new_names,
                         new_devices,
@@ -228,10 +234,88 @@ def test_build_network_dict_1(new_names, network_dict_1, new_devices, new_networ
                         new_monitors,
                         new_scanner)
     
+    # Check that the network dictionary is built correctly
     assert new_parser.network_dict() == network_dict_1
+    # Check that no errors have been accumulated
+    assert new_parser.num_of_errors == 0
 
-### * 5 times the same test, but with different files
-### Test the number of errors too
+@pytest.fixture
+def network_dict_2():
+    network_dict_2 = {}
+    network_dict_2["DEVICES"] = [[Symbol("SWITCH", 6, 1, 10), Symbol("switch1", 16, 1, 17), Symbol("0", 17, 1, 25)], 
+                                 [Symbol("SWITCH", 6, 1, 28), Symbol("switch2", 19, 1, 35), Symbol("0", 17, 1, 43)],
+                                 [Symbol("AND", 0, 1, 46), Symbol("and1", 20, 1, 50), Symbol("2", 21, 1, 55)],
+                                 [Symbol("OR", 1, 1, 58), Symbol("or1", 22, 1, 61), Symbol("2", 21, 1, 65)], 
+                                 [Symbol("NAND", 2, 1, 68), Symbol("nand1", 23, 1, 73), Symbol("2", 21, 1, 79)]]
+    
+    network_dict_2['CONNECT'] = [[Symbol("switch1", 16, 2, 10), Symbol(">", 26, 2, 18), Symbol("and1", 20, 2, 20), Symbol(".", 27, 2, 24), 
+                                  Symbol("I1", 28, 2, 25)], [Symbol("switch1", 16, 2, 29), Symbol(">", 26, 2, 37), Symbol("or1", 22, 2, 39),
+                                  Symbol(".", 27, 2, 42), Symbol("I1", 28, 2, 43)], [Symbol("switch2", 19, 2, 47), Symbol(">", 26, 2, 55), 
+                                  Symbol("and1", 20, 2, 57), Symbol(".", 27, 2, 61), Symbol("I2", 29, 2, 62)], [Symbol("switch2", 19, 2, 66), 
+                                  Symbol(">", 26, 2, 74), Symbol("or1", 22, 2, 76), Symbol(".", 27, 2, 79), Symbol("I2", 29, 2, 80)], 
+                                 [Symbol("and1", 20, 3, 2), Symbol(">", 26, 3, 7), Symbol("nand1", 23, 3, 9), Symbol(".", 27, 3, 14), 
+                                  Symbol("I1", 28, 3, 15)], [Symbol("or1", 22, 3, 19), Symbol(">", 26, 3, 23), Symbol("nand1", 23, 3, 25), 
+                                  Symbol(".", 27, 3, 30), Symbol("I2", 29, 3, 31)]]
+    
+    network_dict_2['MONITOR'] = [[Symbol("and1", 20, 4, 10)], [Symbol("or1", 22, 4, 16)], [Symbol("nand1", 23, 4, 21)]]
+
+    return network_dict_2
+
+def test_build_network_dict_2(network_dict_2, new_names, new_devices, new_network, new_monitors):
+    new_scanner = Scanner("test_parse_files/test_build_network_dict_2.txt", new_names)
+    new_parser = Parser(new_names,
+                        new_devices,
+                        new_network,
+                        new_monitors,
+                        new_scanner)
+
+    # Check that the second test network dictionary is built correctly
+    assert new_parser.network_dict() == network_dict_2
+    # Check that no errors have been accumulated
+    assert new_parser.num_of_errors == 0
+
+@pytest.fixture
+def network_dict_3():
+    network_dict_3 = {}
+    network_dict_3['DEVICES'] = [[Symbol("SWITCH", 6, 1, 10), Symbol("switch1", 16, 1, 17), Symbol("1", 17, 1, 25)],
+                                 [Symbol("SWITCH", 6, 1, 28), Symbol("switch2", 19, 1, 35), Symbol("0", 20, 1, 43)],
+                                 [Symbol("CLOCK", 5, 1, 46), Symbol("clock1", 21, 1, 52), Symbol("2", 22, 1, 59)],
+                                 [Symbol("DTYPE", 7, 1, 62), Symbol("dtype1", 23, 1, 68)], [Symbol("NOR", 3, 2, 10),
+                                  Symbol("nor1", 24, 2, 14), Symbol("2", 22, 2, 19)], [Symbol("XOR", 4, 2, 22), 
+                                  Symbol("xor1", 25, 2, 26)]]
+    network_dict_3['CONNECT'] = [[Symbol("switch1", 16, 3, 10), Symbol(">", 28, 3, 18), Symbol("dtype1", 23, 3, 20),
+                                  Symbol(".", 29, 3, 26), Symbol("DATA", 11, 3, 27)], [Symbol("switch1", 16, 3, 33), 
+                                  Symbol(">", 28, 3, 41), Symbol("dtype1", 23, 3, 43), Symbol(".", 29, 3, 49), 
+                                  Symbol("SET", 9, 3, 50)], [Symbol("switch1", 16, 3, 55), Symbol(">", 28, 3, 63),
+                                  Symbol("nor1", 24, 3, 65), Symbol(".", 29, 3, 69), Symbol("I1", 30, 3, 70)],
+                                 [Symbol("switch2", 19, 4, 1), Symbol(">", 28, 4, 9), Symbol("dtype1", 23, 4, 11), 
+                                  Symbol(".", 29, 4, 17), Symbol("CLEAR", 10, 4, 18)], [Symbol("switch2", 19, 4, 25),
+                                  Symbol(">", 28, 4, 33), Symbol("xor1", 25, 4, 35), Symbol(".", 29, 4, 39), 
+                                  Symbol("I2", 31, 4, 40)], [Symbol("dtype1", 23, 4, 44), Symbol(".", 29, 4, 50),
+                                  Symbol("Q", 12, 4, 51), Symbol(">", 28, 4, 53), Symbol("nor1", 24, 4, 55), 
+                                  Symbol(".", 29, 4, 59), Symbol("I2", 31, 4, 60)], [Symbol("dtype1", 23, 5, 1), Symbol(".", 29, 5, 7), 
+                                  Symbol("QBAR", 13, 5, 8), Symbol(">", 28, 5, 13), Symbol("xor1", 25, 5, 15), Symbol(".", 29, 5, 19),
+                                  Symbol("I1", 30, 5, 20)], [Symbol("clock1", 21, 5, 24), Symbol(">", 28, 5, 31),
+                                  Symbol("dtype1", 23, 5, 33), Symbol(".", 29, 5, 39), Symbol("CLK", 8, 5, 40)]]
+    network_dict_3['MONITOR'] = [[Symbol("switch1", 16, 6, 10)], [Symbol("clock1", 21, 6, 19)], [Symbol("switch2", 19, 6, 27)], 
+                                 [Symbol("dtype1", 23, 6, 36), Symbol(".", 29, 6, 42), Symbol("Q", 12, 6, 43)], 
+                                 [Symbol("nor1", 24, 6, 46)], [Symbol("xor1", 25, 6, 52)]]
+    
+    return network_dict_3
+
+def test_build_network_dict_3(network_dict_3, new_names, new_devices, new_network, new_monitors):
+    new_scanner = Scanner("test_parse_files/test_build_network_dict_3.txt", new_names)
+    new_parser = Parser(new_names,
+                        new_devices,
+                        new_network,
+                        new_monitors,
+                        new_scanner)
+
+    # Check that the third test network dictionary is built correctly
+    assert new_parser.network_dict() == network_dict_3
+    # Check that no errors have been accumulated
+    assert new_parser.num_of_errors == 0
+
 
 @pytest.mark.parametrize("definition_file, symbol_details, message, index, success",
                          [("test_parse_files/semantic_error_1.txt", ["switch1", 16, 1, 35],
